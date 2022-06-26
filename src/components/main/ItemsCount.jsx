8c080f22cc482cbdad34505a38de0ba42c2ac1e4
ClauSaba <claudio.sabatini.argentina@gmail.com>
2022-06-26 19:00:39 -0300

    componentesII

--- src/components/main/ItemsCount.jsx
+++ src/components/main/ItemsCount.jsx
@@ -5,7 +5,7 @@ import	swal	from "sweetalert"
 const ItemsCount = ( {nombre, foto, precio, stock} ) => {
 	const [cantidad, setCantidad] = useState(1);
 
-	const btnCantidad = (valor) => {
+	const btnCantidadSuma = (valor) => {
 		setCantidad(cantidad + valor)
 			cantidad == stock && swal({
 				title: 'No hay mas stock disponible',
@@ -15,16 +15,19 @@ const ItemsCount = ( {nombre, foto, precio, stock} ) => {
   			timerProgressBar: true,
 			});	
 	}
-
+	const btnCantidadResta = (valor) => {
+		setCantidad(cantidad + valor)
+	}
+	
   return (
     <div className='contenedorCartas'>
       <div className="Cartas">
         <img className="FotoProducto" src={foto} alt= {nombre} />    
         <p className="nombrePrecio"> {nombre} - ${precio} </p>
         <div className='cantidadAgregar'>
-          <button onClick={()=>btnCantidad(cantidad==1 ? 0 : -1)}>-</button>
+          <button onClick={()=>btnCantidadResta(cantidad==1 ? 0 : -1)}>-</button>
           <p>{cantidad}</p>
-          <button onClick={()=>btnCantidad((cantidad==stock ? 0 : +1))}>+</button>
+          <button onClick={()=>btnCantidadSuma((cantidad==stock ? 0 : +1))}>+</button>
           <button>Agregar </button>
 				</div>
       </div>
